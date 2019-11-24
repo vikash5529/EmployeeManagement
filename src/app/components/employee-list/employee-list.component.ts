1be21ed6d7fa53cdc12f7990910f24f3c5899115
Vikash Singh <vikash.singh@techverito.com>
2019-11-24 09:16:25 +0530

    1.2 Add employee data

---
+++ src/app/components/employee-list/employee-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from 'src/app/services/employee.service';
+import { Employee } from 'src/app/models/employee';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.scss']
+})
+export class EmployeeListComponent implements OnInit {
+
+  public employeeData: Array<Partial<Employee>> = [];
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit() {
+    this.employeeData = this.employeeService.getEmployeeData();
+    console.log(this.employeeData)
+  }
+
+}
